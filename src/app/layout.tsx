0aacb4bf407152f19400936ad1045373f2b30920
Lavender C <lmcalhoun@wayne.edu>
2025-05-24 05:17:27 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import {Space_Mono} from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: 'Lavender Calhoun',
